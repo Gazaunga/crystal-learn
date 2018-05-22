d5dc5d58e67a925c35d4aea83f26a576949f9d88
Gazaunga <Gazaunga@gmail.com>
2018-05-22 15:48:20 -0400

    Update optparse.cr

--- optparse.cr
+++ optparse.cr
@@ -6,7 +6,7 @@ destination = "World"
 OptionParser.parse! do |parser|
   parser.banner = "Usage: salute [arguments]"
   parser.on("-u", "--upcase", "Upcases the salute") { upcase = true }
-  parser.on("-t NAME", "--to=NAME", "Specifies the name to salute") { |name| destination = name }
+  parser.on("-n NAME", "--name=NAME", "Specifies the name to salute") { |name| destination = name }
   parser.on("-h", "--help", "Show this help") { puts parser }
 end
 
