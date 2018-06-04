8de09ffe77ccf38a42c79b5936f3992004d21db6
Gazaunga <Gazaunga@gmail.com>
2018-06-04 16:23:27 -0400

    Update motd.cr

--- motd.cr
+++ motd.cr
@@ -1,16 +1,18 @@
 #!/usr/bin/env crystal
 
 
+#!/usr/bin/env crystal
+
+class Motd
+@@red    = "\033[31m"
+@@green  = "\033[32m"
+@@yellow = "\033[33m"
+@@blue   = "\033[34m"
+@@reset   = "\033[0m"
+
 def printheader
 
-red    = "\033[31m"
-green  = "\033[32m"
-yellow = "\033[33m"
-blue   = "\033[34m"
-the_end   = "\033[0m"
-
-
-puts "#{blue} _____ #{green}Welcome#{the_end}#{blue} ______ _ _"
+puts "#{@@blue} _____ #{@@green}Welcome#{@@reset}#{@@blue} ______ _ _"
 puts "|  __ \         | ___ (_) |"
 puts "| |  \/ __ _ ___| |_/ /_| |_"
 puts "| | __ / _` |_  / ___ \ | __|"
@@ -21,10 +23,15 @@ end
 
 def sysinfo
 user = %x(id -u -n).chomp
-puts "User:              #{user}"
+puts "#{@@green}User           : #{user}#{@@green}"
+puts "Disk total     : #{%x(df -h /home | tail -1 | awk '{print $2}')}"
+puts "Disk free      : #{%x(df -h /home | tail -1 | awk '{print $4}')}"
+end
+
 end
 
 ##=MAIN=##
+cli = Motd.new
 
-printheader
-sysinfo
+cli.printheader
+cli.sysinfo
