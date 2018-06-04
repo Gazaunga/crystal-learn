1aa72a4ac4b56a8ed05c29b88c8390b94aee1617
Gazaunga <Gazaunga@gmail.com>
2018-06-04 10:57:54 -0400

    Update motd.cr

--- motd.cr
+++ motd.cr
@@ -1,1 +1,22 @@
 
+
+
+def printheader
+
+red    = "\033[31m"
+green  = "\033[32m"
+yellow = "\033[33m"
+blue   = "\033[34m"
+the_end   = "\033[0m"
+
+#puts "#{green}Welcome back#{the_end}#{blue}"
+puts "#{blue} _____ #{green}Welcome#{the_end}#{blue} ______ _ _"
+puts "|  __ \         | ___ (_) |"
+puts "| |  \/ __ _ ___| |_/ /_| |_"
+puts "| | __ / _` |_  / ___ \ | __|"
+puts "| |_\ \ (_| |/ /| |_/ / | |_"
+puts " \____/\__,_/___\____/|_|\__|"
+
+end
+
+printheader
