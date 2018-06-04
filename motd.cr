1e39a3ac7219b32ea307958bb74319f992c56b3e
Gazaunga <Gazaunga@gmail.com>
2018-06-04 13:15:37 -0400

    Update motd.cr

--- motd.cr
+++ motd.cr
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env crystal
 
 
 def printheader
@@ -9,13 +9,22 @@ yellow = "\033[33m"
 blue   = "\033[34m"
 the_end   = "\033[0m"
 
+
 puts "#{blue} _____ #{green}Welcome#{the_end}#{blue} ______ _ _"
 puts "|  __ \         | ___ (_) |"
 puts "| |  \/ __ _ ___| |_/ /_| |_"
 puts "| | __ / _` |_  / ___ \ | __|"
 puts "| |_\ \ (_| |/ /| |_/ / | |_"
 puts " \____/\__,_/___\____/|_|\__|"
-
+puts ""
 end
 
+def sysinfo
+user = %x(id -u -n).chomp
+puts "User:              #{user}"
+end
+
+##=MAIN=##
+
 printheader
+sysinfo
